f019f312d80f3f5a1d67df89347ebc372806384b
licyk <licyk233@163.com>
2024-10-13 11:27:11 +0800

    fix

--- sd_trainer_installer.ps1
+++ sd_trainer_installer.ps1
@@ -1,6 +1,6 @@
 ﻿# 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # SD-Trainer Installer 版本和检查更新间隔
-$SD_TRAINER_INSTALLER_VERSION = 123
+$SD_TRAINER_INSTALLER_VERSION = 124
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_MIRROR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -1840,6 +1840,11 @@ function Main {
         if (!(`$xformers_ver -eq `"`")) {
             Print-Msg `"重装 xFormers 中`"
             if (`$USE_UV) {
+                python -m pip show xformers --quiet 2> `$null
+                if (`$?) {
+                    Print-Msg `"卸载原有 xFormers 中`"
+                    python -m pip uninstall xformers -y
+                }
                 uv pip install `$xformers_ver `$force_reinstall_arg --no-deps
                 if (!(`$?)) {
                     Print-Msg `"检测到 uv 安装 Python 软件包失败, 尝试回滚至 Pip 重试 Python 软件包安装`"
