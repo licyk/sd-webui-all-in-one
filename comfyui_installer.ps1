16939bb994e0a66d9692f1c938c0ba16273267be
licyk <licyk233@163.com>
2024-11-25 18:18:33 +0800

    optimize update result display

--- comfyui_installer.ps1
+++ comfyui_installer.ps1
@@ -1,6 +1,6 @@
 ﻿# 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # ComfyUI Installer 版本和检查更新间隔
-$COMFYUI_INSTALLER_VERSION = 133
+$COMFYUI_INSTALLER_VERSION = 134
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -2853,8 +2853,12 @@ function List-Update-Status (`$update_status) {
             `$failed += 1
         }
         Write-Host `"- `" -ForegroundColor Yellow -NoNewline
-        Write-Host `"`$name`" -ForegroundColor White -NoNewline
-        Write-Host `": `$ver `" -ForegroundColor Cyan
+        Write-Host `"`${name}: `" -ForegroundColor White -NoNewline
+        if (`$status) {
+            Write-Host `"`$ver `" -ForegroundColor Cyan
+        } else {
+            Write-Host `"`$ver `" -ForegroundColor Red
+        }
     }
     Write-Host
     Write-Host `"[●: `$sum | ✓: `$success | ×: `$failed]`" -ForegroundColor White
