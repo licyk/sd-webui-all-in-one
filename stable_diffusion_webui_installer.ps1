ae899991569c5a7a4941c9a2624032d8cdc0a295
licyk <licyk233@163.com>
2024-11-28 12:37:52 +0800

    update

--- stable_diffusion_webui_installer.ps1
+++ stable_diffusion_webui_installer.ps1
@@ -1,6 +1,6 @@
 ﻿# 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # SD WebUI Installer 版本和检查更新间隔
-$SD_WEBUI_INSTALLER_VERSION = 110
+$SD_WEBUI_INSTALLER_VERSION = 111
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -1492,7 +1492,7 @@ function Check-stable-diffusion-webui-Env-Requirements {
 
     `$sum = 0
     ForEach (`$extension_path in `$extension_list) {
-        if (!(Test-Path `"`$extension_path/install.py`")) {
+        if (Test-Path `"`$extension_path/install.py`") {
             `$sum += 1
         }
     }
