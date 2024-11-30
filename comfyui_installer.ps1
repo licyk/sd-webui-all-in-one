d085bb0e8358eafc8758c70977c16f9da791eff1
licyk <licyk233@163.com>
2024-11-30 17:13:58 +0800

    update

--- comfyui_installer.ps1
+++ comfyui_installer.ps1
@@ -5,7 +5,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # ComfyUI Installer 版本和检查更新间隔
-$COMFYUI_INSTALLER_VERSION = 144
+$COMFYUI_INSTALLER_VERSION = 145
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -5966,6 +5966,7 @@ ComfyUI 项目地址：https://github.com/comfyanonymous/ComfyUI
 
 # 写入管理脚本和文档
 function Write-Manager-Scripts {
+    New-Item -ItemType Directory -Path "$InstallPath" -Force > $null
     Write-Launch-Script
     Write-Update-Script
     Write-Update-Node-Script
