26261dbefef06181cec94d0a2c9a1be69b271e7d
licyk <76895225+licyk@users.noreply.github.com>
2024-12-02 14:21:59 +0800

    update model list

--- invokeai_installer.ps1
+++ invokeai_installer.ps1
@@ -5,7 +5,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # InvokeAI Installer 版本和检查更新间隔
-$INVOKEAI_INSTALLER_VERSION = 160
+$INVOKEAI_INSTALLER_VERSION = 161
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -2489,7 +2489,7 @@ function Model-Downloader (`$download_list) {
 
 # 获取用户输入
 function Get-User-Input {
-    return Read-Host `"========================================>`"
+    return Read-Host `"=========================================>`"
 }
 
 
