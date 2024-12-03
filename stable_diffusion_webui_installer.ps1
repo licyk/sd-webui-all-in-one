4f79cd5da031cf748210e7002d198bee9d20fa86
licyk <licyk233@163.com>
2024-12-03 23:22:24 +0800

    fix

--- stable_diffusion_webui_installer.ps1
+++ stable_diffusion_webui_installer.ps1
@@ -12,7 +12,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # SD WebUI Installer 版本和检查更新间隔
-$SD_WEBUI_INSTALLER_VERSION = 128
+$SD_WEBUI_INSTALLER_VERSION = 129
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -3429,7 +3429,7 @@ function Get-Local-Setting {
         }
     }
 
-    if (Test-Path `"`$PSScriptRoot/install_sd_webui.txt`")) {
+    if (Test-Path `"`$PSScriptRoot/install_sd_webui.txt`") {
         `$arg.Add(`"-InstallBranch`", `"sd_webui`")
     } elseif (Test-Path `"`$PSScriptRoot/install_sd_webui_forge.txt`") {
         `$arg.Add(`"-InstallBranch`", `"sd_webui_forge`")
