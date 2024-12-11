56b3f36a93edc4a44d78742ba5843a71aff016b7
licyk <licyk233@163.com>
2024-12-11 18:39:39 +0800

    deprecate

--- invokeai_installer.ps1
+++ invokeai_installer.ps1
@@ -9,7 +9,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # InvokeAI Installer 版本和检查更新间隔
-$INVOKEAI_INSTALLER_VERSION = 175
+$INVOKEAI_INSTALLER_VERSION = 176
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -4711,12 +4711,10 @@ function Main {
         Get-InvokeAI-Installer-Cmdlet-Help
     }
 
-    # TODO: Deprecate Test-Path "$InstallPath/use_update_mode.txt"
-    if ((Test-Path "$InstallPath/use_update_mode.txt") -or ($UseUpdateMode)) {
+    if ($UseUpdateMode) {
         Print-Msg "使用更新模式"
-        Remove-Item -Path "$InstallPath/use_update_mode.txt" -Force -Recurse 2> $null
-        Set-Content -Encoding UTF8 -Path "$InstallPath/update_time.txt" -Value $(Get-Date -Format "yyyy-MM-dd HH:mm:ss") # 记录更新时间
         Use-Update-Mode
+        Set-Content -Encoding UTF8 -Path "$InstallPath/update_time.txt" -Value $(Get-Date -Format "yyyy-MM-dd HH:mm:ss") # 记录更新时间
     } else {
         Print-Msg "使用安装模式"
         Use-Install-Mode
--- stable_diffusion_webui_installer.ps1
+++ stable_diffusion_webui_installer.ps1
@@ -12,7 +12,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # SD WebUI Installer 版本和检查更新间隔
-$SD_WEBUI_INSTALLER_VERSION = 141
+$SD_WEBUI_INSTALLER_VERSION = 142
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -6551,12 +6551,10 @@ function Main {
         Get-Stable-Diffusion-WebUI-Installer-Cmdlet-Help
     }
 
-    # TODO: Deprecate Test-Path "$InstallPath/use_update_mode.txt"
-    if ((Test-Path "$InstallPath/use_update_mode.txt") -or ($UseUpdateMode)) {
+    if ($UseUpdateMode) {
         Print-Msg "使用更新模式"
-        Remove-Item -Path "$InstallPath/use_update_mode.txt" -Force -Recurse 2> $null
-        Set-Content -Encoding UTF8 -Path "$InstallPath/update_time.txt" -Value $(Get-Date -Format "yyyy-MM-dd HH:mm:ss") # 记录更新时间
         Use-Update-Mode
+        Set-Content -Encoding UTF8 -Path "$InstallPath/update_time.txt" -Value $(Get-Date -Format "yyyy-MM-dd HH:mm:ss") # 记录更新时间
     } else {
         Print-Msg "使用安装模式"
         Use-Install-Mode
