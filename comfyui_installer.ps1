bc877e7cc75703245e44bef6e363012af6cecf45
licyk <licyk233@163.com>
2025-02-27 18:29:43 +0800

    update

--- comfyui_installer.ps1
+++ comfyui_installer.ps1
@@ -11,7 +11,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # ComfyUI Installer 版本和检查更新间隔
-$COMFYUI_INSTALLER_VERSION = 196
+$COMFYUI_INSTALLER_VERSION = 197
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -6048,9 +6048,9 @@ function global:Install-Hanamizuki {
                 Move-Item -Path `"`$Env:CACHE_HOME/hanamizuki.exe`" `"`$Env:COMFYUI_INSTALLER_ROOT/ComfyUI/hanamizuki.exe`" -Force
                 `$content = `"
                     @echo off
-                    echo Launch Hanamizuki...
                     cd /d ```"%~dp0```"\ComfyUI
                     if exist .\hanamizuki.exe (
+                        echo Launch Hanamizuki
                         .\hanamizuki.exe
                     ) else (
                         echo Hanamizuki not found
--- sd_trainer_installer.ps1
+++ sd_trainer_installer.ps1
@@ -12,7 +12,7 @@
 )
 # 有关 PowerShell 脚本保存编码的问题: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/about/about_character_encoding?view=powershell-7.4#the-byte-order-mark
 # SD-Trainer Installer 版本和检查更新间隔
-$SD_TRAINER_INSTALLER_VERSION = 224
+$SD_TRAINER_INSTALLER_VERSION = 225
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -5144,9 +5144,9 @@ function global:Install-Hanamizuki {
                 Move-Item -Path `"`$Env:CACHE_HOME/hanamizuki.exe`" `"`$Env:SD_TRAINER_INSTALLER_ROOT/lora-scripts/hanamizuki.exe`" -Force
                 `$content = `"
                     @echo off
-                    echo Launch Hanamizuki...
                     cd /d ```"%~dp0```"\lora-scripts
                     if exist .\hanamizuki.exe (
+                        echo Launch Hanamizuki
                         .\hanamizuki.exe
                     ) else (
                         echo Hanamizuki not found
