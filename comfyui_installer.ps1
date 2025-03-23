aa1ac845c16a69f68f05b1cab32585419d340a73
licyk <licyk233@163.com>
2025-03-23 10:03:45 +0800

    avoid split single arg

--- comfyui_installer.ps1
+++ comfyui_installer.ps1
@@ -31,7 +31,7 @@
 # 在 PowerShell 5 中 UTF8 为 UTF8 BOM, 而在 PowerShell 7 中 UTF8 为 UTF8, 并且多出 utf8BOM 这个单独的选项: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.management/set-content?view=powershell-7.5#-encoding
 $PS_SCRIPT_ENCODING = if ($PSVersionTable.PSVersion.Major -le 5) { "UTF8" } else { "utf8BOM" }
 # ComfyUI Installer 版本和检查更新间隔
-$COMFYUI_INSTALLER_VERSION = 214
+$COMFYUI_INSTALLER_VERSION = 215
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -1197,8 +1197,12 @@ function Get-ComfyUI-Launch-Args {
         } else {
             `$launch_args = Get-Content `"`$PSScriptRoot/launch_args.txt`"
         }
-        `$arguments = [regex]::Matches(`$launch_args, '(`"[^`"]*`"|''[^'']*''|\S+)') | ForEach-Object {
-            `$_.Value -replace '^[`"'']|[`"'']`$', ''
+        if (`$launch_args.Trim().Split().Length -le 1) {
+            `$arguments = `$launch_args.Trim().Split()
+        } else {
+            `$arguments = [regex]::Matches(`$launch_args, '(`"[^`"]*`"|''[^'']*''|\S+)') | ForEach-Object {
+                `$_.Value -replace '^[`"'']|[`"'']`$', ''
+            }
         }
         Print-Msg `"检测到本地存在 launch_args.txt 启动参数配置文件 / -LaunchArg 命令行参数, 已读取该启动参数配置文件并应用启动参数`"
         Print-Msg `"使用的启动参数: `$arguments`"
