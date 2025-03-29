506b67a0e5f5e7e11fbe9ba48d297aa38dc4e6b9
licyk <licyk233@163.com>
2025-03-29 20:13:44 +0800

    fix path

--- comfyui_installer.ps1
+++ comfyui_installer.ps1
@@ -31,7 +31,7 @@
 # 在 PowerShell 5 中 UTF8 为 UTF8 BOM, 而在 PowerShell 7 中 UTF8 为 UTF8, 并且多出 utf8BOM 这个单独的选项: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.management/set-content?view=powershell-7.5#-encoding
 $PS_SCRIPT_ENCODING = if ($PSVersionTable.PSVersion.Major -le 5) { "UTF8" } else { "utf8BOM" }
 # ComfyUI Installer 版本和检查更新间隔
-$COMFYUI_INSTALLER_VERSION = 222
+$COMFYUI_INSTALLER_VERSION = 223
 $UPDATE_TIME_SPAN = 3600
 # Pip 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -666,7 +666,7 @@ function Check-Install {
     if ($NoPreDownloadModel) {
         Print-Msg "检测到 -NoPreDownloadModel 命令行参数, 跳过下载模型"
     } else {
-        $checkpoint_path = "$InstallPath/Fooocus/models/checkpoints"
+        $checkpoint_path = "$InstallPath/ComfyUI/models/checkpoints"
         $url = "https://modelscope.cn/models/licyks/sd-model/resolve/master/sdxl_1.0/Illustrious-XL-v1.1.safetensors"
         $name = Split-Path -Path $url -Leaf
         if (!(Get-ChildItem -Path $checkpoint_path -Include "*.safetensors", "*.pth", "*.ckpt" -Recurse)) {
