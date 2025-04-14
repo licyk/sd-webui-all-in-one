2103820ed2f43438923011b81a2e683712c589fd
licyk <licyk233@163.com>
2025-04-14 19:47:49 +0800

    update extension list

--- stable_diffusion_webui_installer.ps1
+++ stable_diffusion_webui_installer.ps1
@@ -35,7 +35,7 @@
 # 在 PowerShell 5 中 UTF8 为 UTF8 BOM, 而在 PowerShell 7 中 UTF8 为 UTF8, 并且多出 utf8BOM 这个单独的选项: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.management/set-content?view=powershell-7.5#-encoding
 $PS_SCRIPT_ENCODING = if ($PSVersionTable.PSVersion.Major -le 5) { "UTF8" } else { "utf8BOM" }
 # SD WebUI Installer 版本和检查更新间隔
-$SD_WEBUI_INSTALLER_VERSION = 217
+$SD_WEBUI_INSTALLER_VERSION = 218
 $UPDATE_TIME_SPAN = 3600
 # PyPI 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -1092,11 +1092,11 @@ function Get-Stable-Diffusion-WebUI-Extension ($branch) {
         "https://github.com/Akegarasu/sd-webui-model-converter", "$sd_webui_extension_path/sd-webui-model-converter"
     )) | Out-Null
     $sd_webui_extension.Add(@(
-        @("sd_webui", "sd_webui_amdgpu", "sdnext"),
+        @("sd_webui", "sd_webui_amdgpu"),
         "https://github.com/Mikubill/sd-webui-controlnet", "$sd_webui_extension_path/sd-webui-controlnet"
     )) | Out-Null
     $sd_webui_extension.Add(@(
-        @("sd_webui", "sd_webui_amdgpu", "sdnext"),
+        @("sd_webui", "sd_webui_amdgpu"),
         "https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111", "$sd_webui_extension_path/multidiffusion-upscaler-for-automatic1111"
     )) | Out-Null
     $sd_webui_extension.Add(@(
