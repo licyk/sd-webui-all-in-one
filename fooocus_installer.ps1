c03bd88324e21c95fb737e4658f968b0559ca05f
licyk <licyk233@163.com>
2025-05-08 15:32:09 +0800

    update config

--- fooocus_installer.ps1
+++ fooocus_installer.ps1
@@ -33,7 +33,7 @@
 # 在 PowerShell 5 中 UTF8 为 UTF8 BOM, 而在 PowerShell 7 中 UTF8 为 UTF8, 并且多出 utf8BOM 这个单独的选项: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.management/set-content?view=powershell-7.5#-encoding
 $PS_SCRIPT_ENCODING = if ($PSVersionTable.PSVersion.Major -le 5) { "UTF8" } else { "utf8BOM" }
 # Fooocus Installer 版本和检查更新间隔
-$FOOOCUS_INSTALLER_VERSION = 173
+$FOOOCUS_INSTALLER_VERSION = 174
 $UPDATE_TIME_SPAN = 3600
 # PyPI 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -1047,6 +1047,7 @@ function Update-Fooocus-Preset {
         "default_black_out_nsfw" = $false
         "metadata_created_by" = "Fooocus"
         "default_developer_debug_mode_checkbox" = $true
+        "default_describe_apply_prompts_checkbox" = $false
         "default_describe_content_type" = @(
             "Art/Anime"
         )
