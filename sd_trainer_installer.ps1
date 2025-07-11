6597677249650ad39d110cffde9b69a0c351bbd8
licyk <licyk233@163.com>
2025-07-11 16:42:50 +0800

    fix onnxruntime-gpu check

--- sd_trainer_installer.ps1
+++ sd_trainer_installer.ps1
@@ -33,7 +33,7 @@
 # 在 PowerShell 5 中 UTF8 为 UTF8 BOM, 而在 PowerShell 7 中 UTF8 为 UTF8, 并且多出 utf8BOM 这个单独的选项: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.management/set-content?view=powershell-7.5#-encoding
 $PS_SCRIPT_ENCODING = if ($PSVersionTable.PSVersion.Major -le 5) { "UTF8" } else { "utf8BOM" }
 # SD-Trainer Installer 版本和检查更新间隔
-$SD_TRAINER_INSTALLER_VERSION = 288
+$SD_TRAINER_INSTALLER_VERSION = 289
 $UPDATE_TIME_SPAN = 3600
 # PyPI 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -3013,7 +3013,7 @@ def need_install_ort_ver(ignore_ort_install: bool = True) -> OrtType | None:
 
 
 if __name__ == '__main__':
-    print(need_install_ort_ver(True))
+    print(need_install_ort_ver(False))
 `".Trim()
 
     if (Test-Path `"`$PSScriptRoot/lora-scripts/.git`") {
