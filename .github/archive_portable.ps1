552100c4986fdbc9768755dd9ab50f4b973d5fcb
licyk <licyk233@163.com>
2025-08-17 18:07:02 +0800

    reduce portable size

--- .github/archive_portable.ps1
+++ .github/archive_portable.ps1
@@ -11,4 +11,4 @@ $package_name = "${SoftwareName}-${Author}-${build_time}"
 $protable_name = "${package_name}-nightly.7z"
 New-Item -ItemType Directory -Path "${SaveDst}" -Force
 Move-Item -Path "${Source}" -Destination "${Workspace}/${package_name}" -Force
-7z a -t7z -bsp1 "${SaveDst}/${SoftwareName}/${protable_name}" "${Workspace}/${package_name}"
+7z a -t7z -bsp1 -m0=lzma2 -mx=9 -mfb=128 -md=768m -ms=on -mf=BCJ2 "${SaveDst}/${SoftwareName}/${protable_name}" "${Workspace}/${package_name}"
