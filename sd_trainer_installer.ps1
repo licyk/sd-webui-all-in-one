74d58a11340b259f86339eb845807b673e0c645c
licyk <licyk233@163.com>
2025-08-18 17:58:17 +0800

    fix error path

--- sd_trainer_installer.ps1
+++ sd_trainer_installer.ps1
@@ -63,7 +63,7 @@
 # 在 PowerShell 5 中 UTF8 为 UTF8 BOM, 而在 PowerShell 7 中 UTF8 为 UTF8, 并且多出 utf8BOM 这个单独的选项: https://learn.microsoft.com/zh-cn/powershell/module/microsoft.powershell.management/set-content?view=powershell-7.5#-encoding
 $PS_SCRIPT_ENCODING = if ($PSVersionTable.PSVersion.Major -le 5) { "UTF8" } else { "utf8BOM" }
 # SD-Trainer Installer 版本和检查更新间隔
-$SD_TRAINER_INSTALLER_VERSION = 302
+$SD_TRAINER_INSTALLER_VERSION = 303
 $UPDATE_TIME_SPAN = 3600
 # PyPI 镜像源
 $PIP_INDEX_ADDR = "https://mirrors.cloud.tencent.com/pypi/simple"
@@ -4782,7 +4782,7 @@ Main
 function Write-Launch-SD-Trainer-Install-Script {
     $content = "
 param (
-    [string]`$InstallPath = `$PSScriptRoot,
+    [string]`$InstallPath = `$script:PSScriptRoot,
     [switch]`$DisableProxy,
     [string]`$UseCustomProxy,
     [switch]`$DisablePyPIMirror,
