8d8d37dd0445f4ec0af8309efe847a8c3a116d6c
licyk <licyk233@163.com>
2025-09-05 23:25:26 +0800

    (dev) add sd webui manager

--- sd_scripts_ipynb_core.py
+++ sd_scripts_ipynb_core.py
@@ -5030,7 +5030,7 @@ class ComfyUIManager(BaseManager):
         filename: str = None,
         model_type: str | None = "checkpoints",
     ) -> Path | None:
-        """下载大模型
+        """下载模型
 
         :param url`(str)`: 模型的下载链接
         :param filename`(str|None)`: 模型下载后保存的名称
@@ -5038,7 +5038,6 @@ class ComfyUIManager(BaseManager):
         """
         path = self.workspace / self.workfolder / "models" / model_type
         return self.get_model(url=url, path=path, filename=filename, tool="aria2")
-    
 
     def get_sd_model_from_list(
         self,
@@ -5173,10 +5172,7 @@ class ComfyUIManager(BaseManager):
         :param pytorch_mirror`(str|None)`: PyTorch 镜像源链接
         :param comfyui_repo`(str|None)`: ComfyUI 仓库地址
         :param comfyui_requirment`(str|None)`: ComfyUI 依赖文件名
-        :param comfyui_setting`(str|None)`: ComfyUI 预设文件下载链接
-        :param fooocus_translation`(str|None)`: ComfyUI 翻译文件下载地址
-        :param model_downloader`(Literal["aria2","request","mix"])`: 预下载模型时使用的模型下载器
-        :param download_model_thread`(int|None)`: 预下载模型的线程
+        :param comfyui_setting`(str|None)`: ComfyUI 设置文件下载链接
         :param custom_node_list`(list[str])`: 自定义节点列表
         :param check_avaliable_gpu`(bool|None)`: 是否检查可用的 GPU, 当检查时没有可用 GPU 将引发`Exception`
         :param enable_tcmalloc`(bool|None)`: 是否启用 TCMalloc 内存优化
@@ -5218,3 +5214,322 @@ class ComfyUIManager(BaseManager):
         enable_tcmalloc and self.tcmalloc_colab()
         enable_cuda_malloc and self.cuda_malloc.set_cuda_malloc()
         logger.info("ComfyUI 安装完成")
+
+
+
+class SDWebUIManager(BaseManager):
+    """Stable Diffusion WebUI 管理工具"""
+
+    def mount_drive(self) -> None:
+        """挂载 Google Drive 并创建 Stable Diffusion WebUI 输出文件夹"""
+        drive_path = Path("/content/drive")
+        if not (drive_path / "MyDrive").exists():
+            if not self.utils.mount_google_drive(drive_path):
+                raise Exception("挂载 Google Drive 失败, 请尝试重新挂载 Google Drive")
+
+        sd_webui_output = drive_path / "MyDrive" / "sd_webui_output"
+        sd_webui_output.mkdir(exist_ok=True)
+        sd_webui_output_path = self.workspace / self.workfolder / "outputs"
+        try:
+            if sd_webui_output_path.exists():
+                shutil.move(sd_webui_output_path, sd_webui_output_path.parent / f"output_{uuid.uuid4()}")
+            sd_webui_output_path.symlink_to(sd_webui_output)
+        except Exception as e:
+            logger.error("为 Stable Diffusion WebUI 输出目录创建软链接失败: %s", e)
+
+    def get_sd_model(
+        self,
+        url: str,
+        filename: str = None,
+        model_type: str | None = "Stable-diffusion",
+    ) -> Path | None:
+        """下载模型
+
+        :param url`(str)`: 模型的下载链接
+        :param filename`(str|None)`: 模型下载后保存的名称
+        :param model_type`(str|None)`: 模型的类型
+        """
+        if model_type == "embeddings":
+            path = self.workspace / self.workfolder / model_type
+        else:
+            path = self.workspace / self.workfolder / "models" / model_type
+        return self.get_model(url=url, path=path, filename=filename, tool="aria2")
+
+    def get_sd_model_from_list(
+        self,
+        model_list: list[dict[str]],
+    ) -> None:
+        """从模型列表下载模型
+
+        :param path`(str|Path)`: 将模型下载到的本地路径
+        :param model_list`(list[str|int])`: 模型列表
+        :param retry`(int|None)`: 重试下载的次数, 默认为 3
+
+        :notes
+            `model_list`需要指定`url`(模型下载链接), 可选参数为`type`(模型类型), `filename`(模型保存名称), 例如
+            ```python
+            model_list = [
+                {"url": "url1", "type": "Stable-diffusion"},
+                {"url": "url2", "filename": "file.safetensors"},
+                {"url": "url3", "type": "loras", "filename": "lora1.safetensors"},
+                {"url": "url4"},
+            ]
+            ```
+        """
+        for model in model_list:
+            url = model.get("url")
+            filename = model.get("filename")
+            model_type = model.get("type", "Stable-diffusion")
+            self.get_sd_model(url=url, filename=filename, model_type=model_type)
+
+    def install_config(
+        self,
+        setting: str | None = None,
+    ) -> None:
+        """下载 Stable Diffusion WebUI 配置文件
+
+        :param setting`(str|None)`: Stable Diffusion WebUI 设置文件下载链接, 下载后将保存在`{self.workspace}/{self.workfolder}/config.json`
+        """
+        setting_path = self.workspace / self.workfolder
+        logger.info("下载配置文件")
+        setting and self.downloader.download_file(
+            url=setting, path=setting_path, save_name="config.json", tool="aria2")
+
+    def install_extension(
+        self,
+        extension: str,
+    ) -> Path | None:
+        """安装 Stable Diffusion WebUI 扩展
+
+        :param extension`(str)`: 扩展下载地址
+        :return `Path|None`: 扩展安装路径
+        """
+        extension_path = self.workspace / self.workfolder / "extensions"
+        name = os.path.basename(extension)
+        logger.info("安装 %s 扩展中", name)
+        p = GitWarpper.clone(
+                repo=extension,
+                path=extension_path / name
+            )
+        if p is not None:
+            logger.info("安装 %s 扩展完成", name)
+            return p
+        else:
+            logger.error("安装 %s 扩展失败", name)
+            return None
+
+    def install_extensions_from_list(
+        self,
+        extension_list: list[str],
+    ) -> None:
+        """安装 Stable Diffusion WebUI 扩展
+
+        :param extension_list`(list[str])`: 扩展列表
+        """
+        logger.info("安装 Stable Diffusion WebUI 扩展中")
+        for extension in extension_list:
+            self.install_extension(extension)
+        logger.info("安装 Stable Diffusion WebUI 扩展完成")
+
+    def run_extension_installer(self, sd_webui_base_path: Path, extension_dir: Path) -> bool:
+        '''执行扩展依赖安装脚本
+
+        :param sd_webui_base_path`(Path)`: SD WebUI 跟目录, 用于导入自身模块
+        :param extension_dir`(Path)`: 要执行安装脚本的扩展路径
+        :return `bool`: 扩展依赖安装结果
+        '''
+        path_installer = extension_dir / 'install.py'
+        if not path_installer.is_file():
+            return
+
+        try:
+            env = os.environ.copy()
+            py_path = env.get('PYTHONPATH', '')
+            env['PYTHONPATH'] = f'{sd_webui_base_path}{os.pathsep}{py_path}'
+            env['WEBUI_LAUNCH_LIVE_OUTPUT'] = '1'
+            run_cmd(
+                command=[sys.executable, str(path_installer)],
+                custom_env=env,
+            )
+            return True
+        except Exception as e:
+            logger.info('执行 %s 扩展依赖安装脚本时发生错误: %s', extension_dir.name, e)
+            traceback.print_exc()
+            return False
+
+
+    def install_extension_requirements(
+        self,
+        sd_webui_base_path: Path,
+        arg_disable_extra_extensions: bool = False,
+        arg_disable_all_extensions: bool = False,
+    ) -> None:
+        '''安装 SD WebUI 扩展依赖
+
+        :param sd_webui_base_path`(Path)`: SD WebUI 根目录
+        :param arg_disable_extra_extensions`(bool)`: 是否禁用 SD WebUI 额外扩展
+        :param arg_disable_all_extensions`(bool)`: 是否禁用 SD WebUI 所有扩展
+        '''
+        settings_file = sd_webui_base_path / 'config.json'
+        extensions_dir = sd_webui_base_path / 'extensions'
+        builtin_extensions_dir = sd_webui_base_path / 'extensions-builtin'
+        ext_install_list = []
+        ext_builtin_install_list = []
+        settings = {}
+
+        try:
+            with open(settings_file, 'r', encoding='utf8') as file:
+                settings = json.load(file)
+        except Exception as e:
+            logger.warning('Stable Diffusion WebUI 配置文件无效: %s', e)
+
+        disabled_extensions = set(settings.get('disabled_extensions', []))
+        disable_all_extensions = settings.get('disable_all_extensions', 'none')
+
+        if (
+            disable_all_extensions == 'all'
+            or arg_disable_all_extensions
+        ):
+            logger.info('已禁用所有 Stable Diffusion WebUI 扩展, 不执行扩展依赖检查')
+            return
+
+        if extensions_dir.is_dir() and disable_all_extensions != 'extra' and not arg_disable_extra_extensions:
+            ext_install_list = [
+                x
+                for x in extensions_dir.glob('*')
+                if x.name not in disabled_extensions
+                and
+                (x / 'install.py').is_file()
+            ]
+
+        if builtin_extensions_dir.is_dir():
+            ext_builtin_install_list = [
+                x
+                for x in builtin_extensions_dir.glob('*')
+                if x.name not in disabled_extensions
+                and
+                (x / 'install.py').is_file()
+            ]
+
+        install_list = ext_install_list + ext_builtin_install_list
+        extension_count = len(install_list)
+
+        if extension_count == 0:
+            logger.info('无待安装依赖的 Stable Diffusion WebUI 扩展')
+            return
+
+        count = 0
+        for ext in install_list:
+            count += 1
+            ext_name = ext.name
+            logger.info('[%s/%s] 执行 %s 扩展的依赖安装脚本中',
+                        count, extension_count, ext_name)
+            if self.run_extension_installer(
+                sd_webui_base_path=sd_webui_base_path,
+                extension_dir=ext,
+            ):
+                logger.info('[%s/%s] 执行 %s 扩展的依赖安装脚本成功',
+                            count, extension_count, ext_name)
+            else:
+                logger.warning(
+                    '[%s/%s] 执行 %s 扩展的依赖安装脚本失败, 可能会导致该扩展运行异常',
+                    count, extension_count, ext_name
+                )
+
+        logger.info('[%s/%s] 安装 Stable Diffusion WebUI 扩展依赖结束',
+                    count, extension_count)
+
+    def check_env(
+        self,
+        use_uv: bool | None = True,
+        requirements_file: str | None = "requirements_versions.txt",
+    ) -> None:
+        """检查 Stable Diffusion WebUI 运行环境
+
+        :param use_uv`(bool|None)`: 使用 uv 安装依赖
+        :param requirements_file`(str|None)`: 依赖文件名
+        """
+        sd_webui_path = self.workspace / self.workfolder
+        requirement_path = sd_webui_path / requirements_file
+        self.env_check.check_env(requirement_path=requirement_path, name="Stable Diffusion WebUI", use_uv=use_uv)
+        self.install_extension_requirements(sd_webui_base_path=sd_webui_path)
+        self.env_check.fix_torch()
+        self.ort_check.check_onnxruntime_gpu(use_uv=use_uv, ignore_ort_install=True)
+        self.env_check.check_numpy(use_uv=use_uv)
+
+
+    def install(
+        self,
+        torch_ver: str | list | None = None,
+        xformers_ver: str | list | None = None,
+        use_uv: bool | None = True,
+        pypi_index_mirror: str | None = None,
+        pypi_extra_index_mirror: str | None = None,
+        pypi_find_links_mirror: str | None = None,
+        github_mirror: str | list | None = None,
+        huggingface_mirror: str | None = None,
+        pytorch_mirror: str | None = None,
+        sd_webui_repo: str | None = None,
+        sd_webui_requirment: str | None = None,
+        sd_webui_setting: str | None = None,
+        extension_list: list[str] | None = None,
+        check_avaliable_gpu: bool | None = False,
+        enable_tcmalloc: bool | None = True,
+        enable_cuda_malloc: bool | None = True,
+    ) -> None:
+        """安装 Stable Diffusion WebUI
+
+        :param torch_ver`(str|None)`: 指定的 PyTorch 软件包包名, 并包括版本号
+        :param xformers_ver`(str|None)`: 指定的 xFormers 软件包包名, 并包括版本号
+        :param use_uv`(bool|None)`: 使用 uv 替代 Pip 进行 Python 软件包的安装
+        :param pypi_index_mirror`(str|None)`: PyPI Index 镜像源链接
+        :param pypi_extra_index_mirror`(str|None)`: PyPI Extra Index 镜像源链接
+        :param pypi_find_links_mirror`(str|None)`: PyPI Find Links 镜像源链接
+        :param github_mirror`(str|list|None)`: Github 镜像源链接或者镜像源链接列表
+        :param huggingface_mirror`(str|None)`: HuggingFace 镜像源链接
+        :param pytorch_mirror`(str|None)`: PyTorch 镜像源链接
+        :param sd_webui_repo`(str|None)`: Stable Diffusion WebUI 仓库地址
+        :param sd_webui_requirment`(str|None)`: Stable Diffusion WebUI 依赖文件名
+        :param sd_webui_setting`(str|None)`: Stable Diffusion WebUI 预设文件下载链接
+        :param extension_list`(list[str])`: 自定义节点列表
+        :param check_avaliable_gpu`(bool|None)`: 是否检查可用的 GPU, 当检查时没有可用 GPU 将引发`Exception`
+        :param enable_tcmalloc`(bool|None)`: 是否启用 TCMalloc 内存优化
+        :param enable_cuda_malloc`(bool|None)`: 启用 CUDA 显存优化
+        """
+        logger.info("开始安装 Stable Diffusion WebUI")
+        os.chdir(self.workspace)
+        sd_webui_path = self.workspace / self.workfolder
+        sd_webui_repo = "https://github.com/AUTOMATIC1111/stable-diffusion-webui" if sd_webui_repo is None else sd_webui_repo
+        sd_webui_setting = "https://github.com/licyk/sd-webui-all-in-one/raw/main/sd_webui_config.json" if sd_webui_setting is None else sd_webui_setting
+        requirment_path = sd_webui_path / \
+            ("requirements_versions.txt" if sd_webui_requirment is None else sd_webui_requirment)
+        if check_avaliable_gpu and not self.utils.check_gpu():
+            raise Exception(
+                "没有可用的 GPU, 请在 Colab -> 代码执行程序 > 更改运行时类型 -> 硬件加速器 选择 GPU T4\n如果不能使用 GPU, 请尝试更换账号!")
+        self.mirror.set_mirror(
+            pypi_index_mirror=pypi_index_mirror,
+            pypi_extra_index_mirror=pypi_extra_index_mirror,
+            pypi_find_links_mirror=pypi_find_links_mirror,
+            github_mirror=github_mirror,
+            huggingface_mirror=huggingface_mirror
+        )
+        self.mirror.configure_pip()
+        self.env.install_manager_depend(use_uv)
+        self.git.clone(sd_webui_repo, sd_webui_path)
+        if extension_list is not None:
+            self.install_extensions_from_list(extension_list)
+        self.git.update(sd_webui_path)
+        self.env.install_pytorch(
+            torch_package=torch_ver,
+            xformers_package=xformers_ver,
+            pytorch_mirror=pytorch_mirror,
+            use_uv=use_uv,
+        )
+        os.chdir(sd_webui_path)
+        self.env.install_requirements(requirment_path, use_uv)
+        os.chdir(self.workspace)
+        self.install_config(sd_webui_setting)
+        enable_tcmalloc and self.tcmalloc_colab()
+        enable_cuda_malloc and self.cuda_malloc.set_cuda_malloc()
+        logger.info("Stable Diffusion WebUI 安装完成")
+        logger.info(f"asd{logger}")
