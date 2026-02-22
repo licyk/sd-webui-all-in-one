39a31d262cef90d975be7335baa547bbf4e562ce
licyk <licyk233@163.com>
2026-02-22 13:38:25 +0800

    bump version

--- installer/fooocus_installer.ps1
+++ installer/fooocus_installer.ps1
@@ -75,10 +75,10 @@ $script:InstallPath = Join-NormalizedPath $script:InstallPath
     $env:CORE_PREFIX = $target_prefix
 }
 # Fooocus Installer 版本和检查更新间隔
-$script:FOOOCUS_INSTALLER_VERSION = 250
+$script:FOOOCUS_INSTALLER_VERSION = 251
 $script:UPDATE_TIME_SPAN = 3600
 # SD WebUI All In One 内核最低版本
-$script:CORE_MINIMUM_VER = "2.0.29"
+$script:CORE_MINIMUM_VER = "2.0.30"
 # PATH
 & {
     $sep = $([System.IO.Path]::PathSeparator)
--- installer/sd_trainer_installer.ps1
+++ installer/sd_trainer_installer.ps1
@@ -76,10 +76,10 @@ $script:InstallPath = Join-NormalizedPath $script:InstallPath
     $env:CORE_PREFIX = $target_prefix
 }
 # SD Trainer Installer 版本和检查更新间隔
-$script:SD_TRAINER_INSTALLER_VERSION = 357
+$script:SD_TRAINER_INSTALLER_VERSION = 358
 $script:UPDATE_TIME_SPAN = 3600
 # SD WebUI All In One 内核最低版本
-$script:CORE_MINIMUM_VER = "2.0.29"
+$script:CORE_MINIMUM_VER = "2.0.30"
 # PATH
 & {
     $sep = $([System.IO.Path]::PathSeparator)
