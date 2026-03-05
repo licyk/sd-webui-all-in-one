12f2decae9b7a4e9669ce67ca5ca6483b26480b1
licyk <licyk233@163.com>
2026-03-05 17:23:25 +0800

    update fooocus preset

--- installer/fooocus_installer.ps1
+++ installer/fooocus_installer.ps1
@@ -75,10 +75,10 @@ $script:InstallPath = Join-NormalizedPath $script:InstallPath
     $env:CORE_PREFIX = $target_prefix
 }
 # Fooocus Installer 版本和检查更新间隔
-$script:FOOOCUS_INSTALLER_VERSION = 265
+$script:FOOOCUS_INSTALLER_VERSION = 266
 $script:UPDATE_TIME_SPAN = 3600
 # SD WebUI All In One 内核最低版本
-$script:CORE_MINIMUM_VER = "2.0.43"
+$script:CORE_MINIMUM_VER = "2.0.44"
 # PATH
 & {
     $sep = $([System.IO.Path]::PathSeparator)
@@ -818,9 +818,9 @@ function Invoke-Installation {
 
     $target_branch = Get-InstallBranch
     $launch_args_map = @{
-        "fooocus_main"          = "--language zh --preset fooocus_installer --disable-offload-from-vram --disable-analytics --always-download-new-model"
-        "ruined_fooocus_main"   = "--language zh --preset fooocus_installer --disable-offload-from-vram --disable-analytics --always-download-new-model"
-        "fooocus_mre_main"      = "--language zh --preset fooocus_installer --disable-offload-from-vram --disable-analytics --always-download-new-model"
+        "fooocus_main"          = "--language zh --preset sd_webui_all_in_one --disable-offload-from-vram --disable-analytics --always-download-new-model"
+        "ruined_fooocus_main"   = "--language zh --preset sd_webui_all_in_one --disable-offload-from-vram --disable-analytics --always-download-new-model"
+        "fooocus_mre_main"      = "--language zh --preset sd_webui_all_in_one --disable-offload-from-vram --disable-analytics --always-download-new-model"
     }
     if (!(Test-Path (Join-NormalizedPath $script:InstallPath "launch_args.txt"))) {
         Write-Log "设置默认 Fooocus 启动参数"
--- sd_webui_all_in_one/version.py
+++ sd_webui_all_in_one/version.py
@@ -1,3 +1,3 @@
 """SD WebUI All In One 版本"""
 
-VERSION = "2.0.43"
+VERSION = "2.0.44"
