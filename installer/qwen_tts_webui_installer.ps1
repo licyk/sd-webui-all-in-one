bb1ffa1421a5db69bb40031af7b288c561568931
licyk <licyk233@163.com>
2026-06-02 07:06:08 +0000

    Bump installer versions

--- installer/qwen_tts_webui_installer.ps1
+++ installer/qwen_tts_webui_installer.ps1
@@ -220,7 +220,7 @@ $script:HotpatcherPortSet = $PSBoundParameters.ContainsKey("HotpatcherPort")
     $env:CORE_PREFIX = Resolve-CorePrefix -BasePath $script:InstallPath -PrefixList $prefix_list -ConfiguredPrefix $origin_core_prefix
 }
 # Qwen TTS WebUI Installer 版本和检查更新间隔
-$script:QWEN_TTS_WEBUI_INSTALLER_VERSION = 265
+$script:QWEN_TTS_WEBUI_INSTALLER_VERSION = 266
 $script:UPDATE_TIME_SPAN = 3600
 # SD WebUI All In One 内核最低版本
 $script:CORE_MINIMUM_VER = "2.2.36"
