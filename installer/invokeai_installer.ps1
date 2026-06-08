d5a9571fd98400a5dc6b2292b55e2341789ab104
licyk <licyk233@163.com>
2026-06-08 16:00:48 +0000

    bump version

--- installer/invokeai_installer.ps1
+++ installer/invokeai_installer.ps1
@@ -225,10 +225,10 @@ $script:HotpatcherPortSpecified = $PSBoundParameters.ContainsKey("HotpatcherPort
     $env:CORE_PREFIX = Resolve-CorePrefix -BasePath $script:InstallPath -PrefixList $prefix_list -ConfiguredPrefix $origin_core_prefix
 }
 # InvokeAI Installer 版本和检查更新间隔
-$script:INVOKEAI_INSTALLER_VERSION = 466
+$script:INVOKEAI_INSTALLER_VERSION = 467
 $script:UPDATE_TIME_SPAN = 3600
 # SD WebUI All In One 内核最低版本
-$script:CORE_MINIMUM_VER = "2.2.41"
+$script:CORE_MINIMUM_VER = "2.2.42"
 # PATH
 & {
     $sep = $([System.IO.Path]::PathSeparator)
