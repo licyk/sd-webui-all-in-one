861c09d82ae5a6d79b79b09189f21b55f72c7201
licyk <licyk233@163.com>
2026-06-17 14:02:48 +0000

    Bump installer versions

--- installer/sd_trainer_installer.ps1
+++ installer/sd_trainer_installer.ps1
@@ -249,7 +249,7 @@ $script:HotpatcherPortSpecified = $PSBoundParameters.ContainsKey("HotpatcherPort
     $env:CORE_PREFIX = Resolve-CorePrefix -BasePath $script:InstallPath -PrefixList $prefix_list -ConfiguredPrefix $origin_core_prefix
 }
 # SD Trainer Installer 版本和检查更新间隔
-$script:SD_TRAINER_INSTALLER_VERSION = 507
+$script:SD_TRAINER_INSTALLER_VERSION = 508
 $script:UPDATE_TIME_SPAN = 3600
 # SD WebUI All In One 内核最低版本
 $script:CORE_MINIMUM_VER = "2.2.51"
